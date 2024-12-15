dbb99c9c9cae6e8bb7756c27872f52764e58d995
Yurii <yuriydorosh2005@gmail.com>
2024-12-15 00:33:13 +0000

    TEMP: replication db

--- backend/monolith/src/config/settings/components/logging_comp.py
+++ backend/monolith/src/config/settings/components/logging_comp.py
@@ -1,35 +1,61 @@
 import os
-from datetime import datetime
-
 from src.config.settings.components.boilerplate import BASE_DIR
 
-
-LOGGING_DIR = os.path.join(BASE_DIR.parent, "logs")
-
-if not os.path.exists(LOGGING_DIR):
-    os.makedirs(LOGGING_DIR)
-
+LOGGING_DIR_DJANGO = BASE_DIR.parent / "logs" / "django"
+if not LOGGING_DIR_DJANGO.exists():
+    LOGGING_DIR_DJANGO.mkdir(parents=True, exist_ok=True)
 
 LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "[%(asctime)s] [%(levelname)s] [%(process)d] [%(threadName)s] [%(name)s:%(lineno)d] %(message)s",
+            "datefmt": "%Y-%m-%d %H:%M:%S"
+        },
+        "simple": {
+            "format": "[%(asctime)s] %(levelname)s %(message)s",
+            "datefmt": "%Y-%m-%d %H:%M:%S"
+        },
+    },
     "handlers": {
         "console": {
             "class": "logging.StreamHandler",
+            "formatter": "simple",
         },
-        "file": {
+        "error_file": {
             "level": "ERROR",
             "class": "logging.handlers.TimedRotatingFileHandler",
-            "filename": os.path.join(
-                LOGGING_DIR, f"{datetime.now():%Y-%m-%d}.log",
-            ),
+            "formatter": "verbose",
+            "filename": os.path.join(LOGGING_DIR_DJANGO, "error.log"),
+            "when": "midnight",
+            "backupCount": 7,
+            "encoding": "utf-8",
+        },
+        "debug_file": {
+            "level": "DEBUG",
+            "class": "logging.handlers.TimedRotatingFileHandler",
+            "formatter": "verbose",
+            "filename": os.path.join(LOGGING_DIR_DJANGO, "debug.log"),
             "when": "midnight",
             "backupCount": 7,
             "encoding": "utf-8",
         },
     },
+    "loggers": {
+        "django": {
+            "handlers": ["console", "error_file", "debug_file"],
+            "level": "DEBUG",
+            "propagate": True,
+        },
+        "django.server": {
+            "handlers": ["console", "error_file", "debug_file"],
+            "level": "INFO",
+            "propagate": False,
+        },
+    },
     "root": {
-        "handlers": ["console", "file"],
+        "handlers": ["console", "error_file", "debug_file"],
         "level": "ERROR",
     },
 }
