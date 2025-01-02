b740bd2ae80c75bd0df864b188293782d26255a9
Yurii <yuriydorosh2005@gmail.com>
2025-01-02 19:10:21 +0000

    FIX: minor fixes in docker configs

--- docker_compose/worker_1/entrypoint.sh
+++ docker_compose/worker_1/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-celery -A src.celery_app.app worker -l info
+celery -A src.celery_app.app worker -l info --hostname=worker1@%h
--- docker_compose/worker_3/entrypoint.sh
+++ docker_compose/worker_3/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-celery -A src.celery_app.app worker -l info --concurrency=3
+celery -A src.celery_app.app worker -l info --concurrency=2 --hostname=worker3@%h
